714a6757a54dc2538c84a05f81f3724574602eac
Aitor <helloimaitor@gmail.com>
2019-12-15 10:19:44 +0000

    closure example

---
+++ closure/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// closure helps to limit the scope of variables used by multiple functions
+func wrapper() func() int {
+	i := 10
+	return func() int {
+		i--
+		return i
+	}
+}
+
+func main() {
+	i := 0
+	add := func() int {
+		i++
+		return i
+	}
+
+	fmt.Println(add())
+	fmt.Println(add())
+
+	sub := wrapper()
+	fmt.Println(sub())
+	fmt.Println(sub())
+}
