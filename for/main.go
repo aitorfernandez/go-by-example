0e58b29875c9d1200ab2b6b5662330a4178a8d22
Aitor <helloimaitor@gmail.com>
2019-12-16 09:17:04 +0000

    add foor loop

---
+++ for/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	// old school for loop
+	// for init; condition; post {
+	// }
+
+	i := 0
+	// C's while
+	for {
+		i++
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Println(i)
+		if i >= 50 {
+			break
+		}
+	}
+
+	// labels
+out:
+	for i := 0; i < 300; i++ {
+		// []byte deals with bytes, that is, only 1 byte (8 bits) at a time
+		fmt.Printf("%v, %v, %v, \n", i, string(i), []byte(string(i)))
+		if i == 250 {
+			break out
+		}
+	}
+}
