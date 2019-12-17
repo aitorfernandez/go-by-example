4ee8ba3939f57abf63e845ae6297f704e64d8400
Aitor <helloimaitor@gmail.com>
2019-12-17 11:14:08 +0000

    add arrays

---
+++ arrays/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	// a := [7]int{3, 5, 7, 9, 11, 13, 17}
+	a := [...]int{3, 5, 7, 9, 11, 13, 17}
+	fmt.Println(a)
+
+	// old school
+	// for i := 0; i < len(a); i++ {
+	for i, l := 0, len(a); i < l; i++ {
+		fmt.Println(a[i])
+	}
+
+	for _, v := range a {
+		fmt.Println(v)
+	}
+
+	// multidimensional array
+	arr := [2][2]int{
+		{3, 5},
+		{7, 9},
+	}
+
+	for _, r := range arr {
+		for _, c := range r {
+			fmt.Print(c, " ")
+		}
+		fmt.Println()
+	}
+}
