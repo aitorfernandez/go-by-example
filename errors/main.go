ab2795bef4b59f5b6d4d5e35129e6f81625bdc8b
Aitor <helloimaitor@gmail.com>
2019-12-18 18:37:49 +0000

    fix error name

--- errors/main.go
+++ errors/main.go
@@ -6,10 +6,10 @@ import (
 	"math"
 )
 
-// Curstom
-var NegativeErr = errors.New("negative number")
+// Custom
+var ErrNegative = errors.New("negative number")
 
-// Or even more customs
+// Or even more custom
 type NegativeMathErr struct {
 	code int
 	err  error
@@ -26,7 +26,7 @@ func main() {
 func sqrt(f float64) (float64, error) {
 	if f < 0 {
 		// return 0, errors.New("negative number")
-		// return 0, NegativeErr
+		// return 0, ErrNegative
 		// return 0, fmt.Errorf("negative number: %v", f)
 		m := fmt.Errorf("negative number: %v", f)
 		return 0, &NegativeMathErr{"505", m}
