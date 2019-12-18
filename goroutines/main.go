cb79f6aea065f694624e1fa9e5d079837a0faf70
Aitor <helloimaitor@gmail.com>
2019-12-18 22:02:36 +0000

    add channels example

--- goroutines/main.go
+++ goroutines/main.go
@@ -15,11 +15,22 @@ func init() {
 }
 
 func main() {
-	wg.Add(2)
-	// With a goroutine, return immediately to the next line and don’t wait for the function to complete
-	go foo()
-	go bar()
-	wg.Wait()
+	// wg.Add(2)
+	// // With a goroutine, return immediately to the next line and don’t wait for the function to complete
+	// go foo()
+	// go bar()
+	// wg.Wait()
+
+	// Channels provide a way for two goroutines to communicate with each other and syn‐ chronize their execution
+	// {string, int...} the things that are passed on the channel
+	var c chan string = make(chan string)
+	// Using a channel like this synchronizes the two goroutines. When pinger attempts to send a message on the channel, it will wait until printer is ready to receive the message
+	go pinger(c)
+	go ponger(c)
+	go printer(c)
+
+	var input string
+	fmt.Scanln(&input)
 }
 
 func foo() {
@@ -38,4 +49,27 @@ func bar() {
 		time.Sleep(amt * time.Millisecond)
 	}
 	wg.Done()
+}
+
+// Now pinger is only allowed to send to c
+// func pinger(c chan<- string) { ...
+func pinger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "ping"
+	}
+}
+
+func ponger(c chan string) {
+	for i := 0; ; i++ {
+		c <- "pong"
+	}
+}
+
+// bidirectional channel
+func printer(c chan string) {
+	for {
+		msg := <-c
+		fmt.Println(msg)
+		time.Sleep(time.Second * 1)
+	}
 }
